f09616dd22a1d8155f879c4a62cc229080d4c1b2
joshkestenberg <joshkestenberg@gmail.com>
2017-02-15 15:42:18 -0500

    assign complete

--- js/main.js
+++ js/main.js
@@ -1,2 +1,12 @@
-
-
+$(function(){
+  $('body').on('click', 'button', function(){
+    $('body').toggleClass('dark');
+    if ($('button').attr('class')==='on'){
+      $('button').attr('class', 'off');
+      $('.status').text('Who turned out the lights?!');
+    } else {
+      $('button').attr('class', 'on');
+      $('.status').text("It's so bright in here!");
+    }
+  });
+});
